adec231bd37c00d4cfbe0371b53d1f53ece37af6
Negar Morovatian <97600041+Negmor@users.noreply.github.com>
2024-04-08 14:02:00 +0330

    sessions(add product to basket without register

--- home/views.py
+++ home/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+
 # Create your views here.
 class Home(TemplateView):
-    template_name ="home/index.html"
+    template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+
+        self.request.session.get("my_name","amir")
+
+        #self.request.session["my_name"] = "amir"
+        #del self.request.session["my_name"]
+        #print(self.request.session["my_name"])
+        return context
+
+
