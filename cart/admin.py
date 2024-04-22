4d5b810f582eff2ee6c2a72e764c2ef3c56e2337
Negar Morovatian <97600041+Negmor@users.noreply.github.com>
2024-04-22 12:59:41 +0330

    basket(cart)_check out _model

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from . import models
+
+class orderItem(admin.TabularInline):
+    model=models.OrderItem
+
+
+
+@admin.register(models.Order)
+class orderAdmin(admin.ModelAdmin):
+    list_display = ("user","adress","phone","is_paid")
+    inlines = (orderItem,)
+    list_filter = ("is_paid",)
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+from account.models import User
+from product.models import Product
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    adress = models.CharField(max_length=300)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=12)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.phone
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="items")
+    size = models.CharField(max_length=12)
+    color = models.CharField(max_length=12)
+    quantity = models.SmallIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.order.phone
