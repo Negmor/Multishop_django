b2e89ae4ca6fd39d26a0321dcc595545c50ca618
Negar Morovatian <97600041+Negmor@users.noreply.github.com>
2024-04-24 18:46:34 +0330

    papal

--- cart/models.py
+++ cart/models.py
@@ -10,6 +10,7 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     total = models.IntegerField(default=0)
     is_paid = models.BooleanField(default=False)
+    address=models.TextField(default="")
 
     def __str__(self):
         return self.user.phone
