f728900ef3579af2f84ec59057fadcd025303a1b
Negar Morovatian <97600041+Negmor@users.noreply.github.com>
2024-04-28 12:30:52 +0330

    filters

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import DetailView, TemplateView,ListView
 from product.models import Product, Category
 import templates
 
@@ -28,6 +28,28 @@ class CategoryStyle(TemplateView):
         return context
 
 
-class ProductListview(TemplateView):
+class ProductListview(ListView):
     template_name = "product/product_list.html"
+    queryset = Product.objects.all()
+
+    def get_context_data(self, **kwargs):
+        request=self.request
+        color = request.GET.getlist("color")
+        size = request.GET.getlist("size")
+        min_price = request.GET.get("min_price")
+        max_price = request.GET.get("max_price")
+        queryset = Product.objects.all()
+        if color:
+            queryset=queryset.filter(color__title__in=color).distinct()
+        if size:
+            queryset = queryset.filter(size__title__in=size).distinct()
+        if min_price and max_price:
+            queryset = queryset.filter(price__lte=max_price,price__gte=min_price).distinct()
+
+        context = super(ProductListview, self).get_context_data()
+        context["object_list"] = queryset
+        print(queryset)
+        return context
+
+
 
